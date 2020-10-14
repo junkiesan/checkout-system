0d056cca9e77929dc2a801ea3822c343c5b55a3d
Julien Bouland <bouland.julien@gmail.com>
2020-10-14 16:28:00 +0200

    corrected tests for integration specs test

--- spec/features/integration_spec.rb
+++ spec/features/integration_spec.rb
@@ -2,14 +2,23 @@ require_relative '../../lib/checkout'
 require_relative '../../lib/item'
 
 describe 'Integration Specs' do
-  let(:products) do
-    [
-      Item.new(name: 'A', price: 30),
-      Item.new(name: 'B', price: 20),
-      Item.new(name: 'C', price: 50),
-      Item.new(name: 'D', price: 15)
-    ]
-  end
+  # let(:products) do
+  #   [
+  #     Item.new(name: 'A', price: 30),
+  #     Item.new(name: 'B', price: 20),
+  #     Item.new(name: 'C', price: 50),
+  #     Item.new(name: 'D', price: 15)
+  #   ]
+  # end
+  let(:products) { Item.new(name, price) }
+  let(:name) { "A" }
+  let(:price) { 30 }
+  let(:name) { "B" }
+  let(:price) { 20 }
+  let(:name) { "C" }
+  let(:price) { 50 }
+  let(:name) { "D" }
+  let(:price) { 15 }
   subject(:checkout) { Checkout.new products: products }
 
   it 'Test a £20 discount on basket if price > £150' do
